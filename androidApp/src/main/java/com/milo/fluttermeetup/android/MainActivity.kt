043a20cf70adaa03a276e20de9442a7377149650
SamIAmHarris <sam@groundcrewdev.com>
2023-12-06 15:15:46 -0500

    Update Android UI

--- androidApp/src/main/java/com/milo/fluttermeetup/android/MainActivity.kt
+++ androidApp/src/main/java/com/milo/fluttermeetup/android/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.milo.fluttermeetup.android
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshots.SnapshotApplyResult
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.milo.fluttermeetup.Greeting
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.milo.fluttermeetup.SpaceXSDK
 import com.milo.fluttermeetup.cache.DatabaseDriverFactory
 import com.milo.fluttermeetup.entity.RocketLaunch
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +44,11 @@ class MainActivity : ComponentActivity() {
                 mainScope.launch {
                     kotlin.runCatching {
                         launchesContent.value = Resource.Loading
+                        delay(3000)
                         sdk.getLaunches(needReload)
                     }.onSuccess {
                         launchesContent.value = Resource.Content(it)
                     }.onFailure {
-                        Log.i("Launch", "Failed to get launches: $it")
                         launchesContent.value = Resource.Error
                     }
                 }
@@ -49,17 +57,39 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
-                    when (launchesContent.value) {
-                        Resource.Uninitialized ->
-                            Button(onClick = { displayLaunches(false) }) {
-                                Text(text = "Get Launches")
+
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        when (launchesContent.value) {
+                            Resource.Uninitialized ->
+                                Button(onClick = { displayLaunches(false) }) {
+                                    Text(text = "Get Launches", color = Color.White)
+                                }
+
+                            is Resource.Content -> {
+                                val launches = (launchesContent.value as Resource.Content).launches
+                                Column {
+                                    Text(
+                                        text = "SpaceX Launches",
+                                        fontSize = 44.sp,
+                                        color = Color.Black
+                                    )
+                                    LazyColumn {
+                                        items(launches.size) { index ->
+                                            val launch = launches[index]
+                                            LaunchRow(launch)
+                                        }
+                                    }
+                                }
                             }
 
-                        is Resource.Content -> Text("Received data: ${(launchesContent.value as Resource.Content).launches}")
-                        Resource.Error -> Text("Failed to load launches")
-                        Resource.Loading -> Text("Loading")
+                            Resource.Error -> Text("Failed to load launches")
+                            Resource.Loading -> CircularProgressIndicator()
+                        }
                     }
                 }
             }
@@ -80,15 +110,54 @@ sealed class Resource {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
+fun LaunchRow(launch: RocketLaunch) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxSize()
+                .background(color = Color.White)
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+            Text(
+                text = "Launch name ${launch.missionName}",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
+            val success = launch.launchSuccess ?: false
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = if (success) "Unsuccessful" else "Successful",
+                color = if (success) Color.Green else Color.Red,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Launch year: ${launch.launchYear}",
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Launch details: ${launch.details}",
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Divider(color = Color.Black)
+        }
     }
 }
 
