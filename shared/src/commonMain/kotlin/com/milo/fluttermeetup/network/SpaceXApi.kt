f145f391f0c83f190f1c866d7be6739162ebfea5
SamIAmHarris <sam@groundcrewdev.com>
2023-12-07 09:48:26 -0500

    Limit data shown to user from SpaceX

--- shared/src/commonMain/kotlin/com/milo/fluttermeetup/network/SpaceXApi.kt
+++ shared/src/commonMain/kotlin/com/milo/fluttermeetup/network/SpaceXApi.kt
@@ -5,7 +5,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.request.setBody
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 class SpaceXApi {
@@ -22,3 +24,9 @@ class SpaceXApi {
         return httpClient.get("https://api.spacexdata.com/v5/launches").body()
     }
 }
+
+@Serializable
+data class Data(val options: Options)
+
+@Serializable
+data class Options(val limit: Int)
